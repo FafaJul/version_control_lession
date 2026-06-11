bfa9c26eb2eabf4bd4d6ba3040eaa5ba27bb51f8
FafaJul <trygubiuliia@gmail.com>
2026-06-11 17:32:21 +0300

    comment deleted

--- analytics.py
+++ analytics.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#create dffrom dictionary
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Age': [22,34,45]})
 print(df.head())
 sns.barplot(data=df, x='Name', y='Age')
