d57ff9b1ce512bcc62f43c2be9628faca4db3fb0
FafaJul <trygubiuliia@gmail.com>
2026-06-11 13:27:00 +0300

    changes to analytics.py, comments added

--- analytics.py
+++ analytics.py
@@ -1,4 +1,6 @@
+#import libraries
 import pandas as pd
 import numpy as np
+#create dffrom dictionary
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Age': [22,34,45]})
-df.head()
+print(df.head())
